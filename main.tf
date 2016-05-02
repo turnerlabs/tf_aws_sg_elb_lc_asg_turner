fc83a356d1f630a21d8083bc9576c3ec33056911
Josh Kurz <josh.kurz@turner.com>
2016-05-02 14:48:37 -0400

    fix(main): remove ingress block
    Signed-off-by: Josh Kurz <josh.kurz@turner.com>

--- main.tf
+++ main.tf
@@ -62,14 +62,6 @@ resource "aws_security_group" "sg_instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow inbound from VPC
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["${var.vpc_cidr_block}"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
